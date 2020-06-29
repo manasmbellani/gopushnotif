99bff564dd0cc08870086db9990049f4351e16eb
Manas Bellani <edwards.aus@hotmail.com>
2020-06-29 16:22:04 +1000

    added a README file with some examples, added an initial script to send pushover notifications

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # gopushnotif
-Golang script to send notification to the pushover application (along with URL screenshots where message provided in "[id] &lt;url>" format)
+Golang script to send notification to Pushover application (along with URL screenshots where message provided in "[id] <url>" format)
+
+Currently, `gowitness` is used to take screenshot and all screenshots are stored in folder `out-screenshots`. Plan is to make this run concurrently in near-future.
+
+## Examples
+
+Assuming that we have stored the Pushover notification flags in env vars `PUSHOVER_APP_TOKEN` and `PUSHOVER_USER_KEY`, below are some examples of how to use this.
+
+Here is a simple example of sending message `Hello World` via pushover. 
+```
+$ echo -e "Hello World." | go run /opt/athena-tools/notify/gopushnotif.go -t $PUSHOVER_APP_TOKEN -u $PUSHOVER_USER_KEY -p
+```
+
+To send message with `[id] <url>` with a screenshot of URL, add a `-p` flag
+```
+$ echo -e "[test] https://www.google.com\n[test2] https://www.msn.com" | go run /opt/athena-tools/notify/gopushnotif.go -t $PUSHOVER_APP_TOKEN -u $PUSHOVER_USER_KEY -p
+```
---
+++ gopushnotif.go
@@ -0,0 +1,235 @@
+// Function to send go push notification to pushover app.
+// It also recognises special lines in the format [sig] urls if `-p` flag is set
+// and takes a screenshot via gowitness (which should be installed in PATH dir)
+//
+
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"reflect"
+	"regexp"
+	"strings"
+
+	"github.com/gregdel/pushover"
+)
+
+const SCREENSHOTS_FOLDER = "out-screenshots-743837871"
+const SCREENSHOT_FILE_NAME = "out-screenshot.png"
+
+func getRegexGroups(regEx, url string) (paramsMap map[string]string) {
+
+	var compRegEx = regexp.MustCompile(regEx)
+	match := compRegEx.FindStringSubmatch(url)
+
+	paramsMap = make(map[string]string)
+	for i, name := range compRegEx.SubexpNames() {
+		if i > 0 && i <= len(match) {
+			paramsMap[name] = match[i]
+		}
+	}
+	return paramsMap
+}
+
+// Determine if line mentioned is a url with HTTP/HTTPS protocol supplied
+func isURLWithHTTPProtocol(line string) bool {
+	if strings.Index(line, "http://") != -1 ||
+		strings.Index(line, "https://") != -1 {
+		return true
+	} else {
+		return false
+	}
+}
+
+// Execute a command to get the output, error. Command is executed when in the
+// optionally specified 'cmdDir' OR it is executed with the current working dir
+func execCmd(cmdToExec string, cmdDir string) string {
+	// Get the original working directory
+	owd, _ := os.Getwd()
+
+	// Switch to the directory
+	if cmdDir != "" {
+		os.Chdir(cmdDir)
+	}
+
+	// Get my current working directory
+	cwd, _ := os.Getwd()
+
+	log.Printf("[v] Executing cmd: %s in dir: %s\n", cmdToExec, cwd)
+
+	cmd := exec.Command("/bin/bash", "-c", cmdToExec)
+	out, err := cmd.CombinedOutput()
+	var outStr, errStr string
+	if out == nil {
+		outStr = ""
+	} else {
+		outStr = string(out)
+	}
+
+	if err == nil {
+		errStr = ""
+	} else {
+		errStr = string(err.Error())
+		//log.Printf("Command Error: %s\n", err)
+	}
+
+	totalOut := (outStr + "\n" + errStr)
+
+	// Switch back to the original working directory
+	os.Chdir(owd)
+
+	return totalOut
+}
+
+// Take screenshot utilising 'gowitness' and return screenshot output file path
+func takeScreenshot(url string, screenshotName string, gowitnessBin string) string {
+
+	// Default to screenshot.png if not specified
+	if screenshotName == "" {
+		screenshotName = "screenshot.png"
+	}
+
+	// Prepare screenshot template by clearing screenshots from the folder and
+	// then take gowitness in the folder
+	screenshotTmp := "rm {screenshots_folder}/*; "
+	screenshotTmp += "{gowitness} single --url {url} -d {screenshots_folder}; "
+	screenshotTmp += "mv {screenshots_folder}/*.png {screenshots_folder}/{screenshot_name}"
+
+	// Create the screenshot folder if it doesn't exist
+	_, err := os.Stat(SCREENSHOTS_FOLDER)
+	if os.IsNotExist(err) {
+		os.Mkdir(SCREENSHOTS_FOLDER, 0755)
+	}
+
+	if isURLWithHTTPProtocol(url) {
+		// Take the screenshot
+		screenshotCmd := screenshotTmp
+		screenshotCmd = strings.ReplaceAll(screenshotCmd, "{screenshots_folder}",
+			SCREENSHOTS_FOLDER)
+		screenshotCmd = strings.ReplaceAll(screenshotCmd, "{url}", url)
+		screenshotCmd = strings.ReplaceAll(screenshotCmd, "{screenshot_name}",
+			SCREENSHOT_FILE_NAME)
+		screenshotCmd = strings.ReplaceAll(screenshotCmd, "{gowitness}",
+			gowitnessBin)
+		outfile := path.Join(SCREENSHOTS_FOLDER, SCREENSHOT_FILE_NAME)
+
+		execCmd(screenshotCmd, "")
+
+		return outfile
+
+	} else {
+		return ""
+	}
+
+}
+
+// Worker function that will send a notification reading from the channel
+//unc goSendNotf(app *pushover.Pushover, recipient *pushover.Pushover, msgs chan string, attachment string) {
+//	for msg := range msgs {
+//
+//	}
+//
+//}
+
+func main() {
+	userKeyPtr := flag.String("u", "", "Pushover User key")
+	appTokenPtr := flag.String("t", "", "Pushover App Token")
+	attachmentPtr := flag.String("a", "", "Attachment path")
+	parseSignaturePtr := flag.Bool("p", false,
+		"Parse signature of format '[id]: url', and send screenshot if URL of form https://,http:// detected")
+	verbosePtr := flag.Bool("v", false, "Verbose message")
+	gowitnessPtr := flag.String("g", "gowitness",
+		"Path to gowitness to take screenshot")
+	flag.Parse()
+	userKey := *userKeyPtr
+	appToken := *appTokenPtr
+	attachment := *attachmentPtr
+	parseSignature := *parseSignaturePtr
+	verbose := *verbosePtr
+	gowitness := *gowitnessPtr
+
+	if appToken == "" {
+		log.Fatalf("[-] App Token must be specified")
+	}
+
+	if userKey == "" {
+		log.Fatalf("[-] User Key must be specified")
+	}
+
+	// Decide whether to display verbose log messages, or not
+	if !verbose {
+		log.SetFlags(0)
+		log.SetOutput(ioutil.Discard)
+	}
+
+	// Create a new Pushover App with a token
+	app := pushover.New(appToken)
+	fmt.Println(reflect.TypeOf(app))
+
+	// Create a new recipient with the user key
+	recipient := pushover.NewRecipient(userKey)
+
+	// Read signatures/lines to process from input
+	sc := bufio.NewScanner(os.Stdin)
+	for sc.Scan() {
+		line := sc.Text()
+		if line != "" {
+
+			url := ""
+			outfile := ""
+			if parseSignature {
+				// Attempt to parse the signature into ID, and URL field
+				m := getRegexGroups(`\[(?P<id>[a-zA-Z0-9\_\.\-]+)\]\s*(?P<url>.+)`,
+					line)
+				if len(m) > 0 {
+					regexurl := m["url"]
+					if isURLWithHTTPProtocol(regexurl) {
+						url = regexurl
+
+						log.Printf("Taking screenshot of URL: %s\n", url)
+						outfile = takeScreenshot(url, SCREENSHOT_FILE_NAME,
+							gowitness)
+					}
+				}
+			}
+
+			// Create the message to send
+			message := pushover.NewMessage(line)
+			if attachment != "" {
+				file, _ := os.Open(attachment)
+				message.AddAttachment(bufio.NewReader(file))
+			}
+
+			// Attach the output file as well
+			if outfile != "" {
+				file, _ := os.Open(outfile)
+				message.AddAttachment(bufio.NewReader(file))
+			}
+
+			// Send the image, and the response details too
+			response, err := app.SendMessage(message, recipient)
+			if err != nil {
+				log.Panic(err)
+			}
+
+			// Print the Pushover API response
+			log.Printf("Pushover API Response: %+v", response)
+
+			// Print any error messages
+			if err != nil {
+				log.Panic(err)
+			}
+
+			// Print the input message as-is to output
+			fmt.Println(line)
+
+		}
+	}
+}
